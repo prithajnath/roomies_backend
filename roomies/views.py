2bb5e254765bf5d5308273cd035a0c43e0fdc1ea
Prithaj <prithajnath@gmail.com>
2017-03-07 09:04:39 +0000

    Fix typo in serializer class

--- roomies/views.py
+++ roomies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProfileSerlializer
+from .serializers import ProfileSerializer
 # Create your views here.
 
 class ProfileStuff(APIView):
