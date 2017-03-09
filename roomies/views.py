dd86a7f892e99025980e407d2e4d56c893a78c35
Prithaj <prithajnath@gmail.com>
2017-03-09 01:58:38 +0000

    Finish bare bones user registration

--- roomies/views.py
+++ roomies/views.py
@@ -5,19 +5,25 @@ from rest_framework import status
 from .serializers import ProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken import views as rest_framework_views
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 # Create your views here.
 
 class ProfileStuff(APIView):
     def get(self,request):
         return Response(ProfileSerializer(request.user).data)
-        
+
+@permission_classes((AllowAny, ))      
 class SignUp(APIView):
     def post(self,request):
         username = request.POST['username']
+        print username
         password = request.POST['password']
         email = request.POST['email']
         user = User.objects.create_user(username,email,password)
         user.save()
+        return Response(ProfileSerializer(user).data)
         #return token for new user
         
             
